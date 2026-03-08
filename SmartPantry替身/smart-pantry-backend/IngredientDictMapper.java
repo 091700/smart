73095f47d5f056dfcfae4b1e60e1b03e26d076b8
091700 <1020040917@qq.com>
2026-03-08 23:36:45 +0800

    Delete SmartPantry替身 directory

--- SmartPantry替身/smart-pantry-backend/IngredientDictMapper.java
+++
@@ -1,7 +0,0 @@
-package com.pantry.mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.pantry.entity.IngredientDict;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface IngredientDictMapper extends BaseMapper<IngredientDict> {}
